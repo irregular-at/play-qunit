dc23e8ea6b06602085b66970e2702853b7293461
Florian Gutmann <f.gutmann@irregular.at>
2011-05-13 10:52:34 +0200

    Removed unused include

--- app/controllers/qunit/QUnit.java
+++ app/controllers/qunit/QUnit.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.regex.Pattern;
 
 import models.qunit.TestResult;
 
