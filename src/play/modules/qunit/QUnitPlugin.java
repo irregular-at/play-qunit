6ad5bd11ce7e275a0872e5384ebd19d326a782a5
Florian Gutmann <f.gutmann@irregular.at>
2011-07-01 11:31:40 +0200

    Enable qunit tests also in prod mode when id is test

--- src/play/modules/qunit/QUnitPlugin.java
+++ src/play/modules/qunit/QUnitPlugin.java
@@ -8,7 +8,9 @@ import play.mvc.Router;
 public class QUnitPlugin extends PlayPlugin {
 	
 	public void onRoutesLoaded() {
-		if(! Play.mode.isProd()) {
+		if(!Play.mode.isProd() 
+				|| "test".equals(Play.id)
+				|| (Play.id.startsWith("test-") && Play.id.length() > 5)) {
 			Router.addRoute("GET", "/@qunit", "qunit.Qunit.list");
 			Router.addRoute("GET", "/@qunit/run{<.*>test}", "qunit.Qunit.run");
 			Router.addRoute("POST", "/@qunit/result", "qunit.Qunit.result");
